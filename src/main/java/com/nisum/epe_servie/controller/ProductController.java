c96d60825660c5fd3d07bbcc001f7890fbfb460e
Jeevan Kumar <kumarjeevan1020@gmail.com>
2025-02-04 08:54:13 +0530

    third-commit

--- src/main/java/com/nisum/epe_servie/controller/ProductController.java
+++ src/main/java/com/nisum/epe_servie/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.nisum.epe_servie.controller;
 
 import com.nisum.epe_servie.model.Product;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +15,16 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/products")
+@Slf4j
 public class ProductController {
+
     private List<Product> products = new ArrayList<>();
 
     @GetMapping(value = "/allProducts")
     public ResponseEntity<List<Product>> allProducts(){
+        log.info("Initializing start with all products: {}");
+
+
         List<Product> allProdeucts = Arrays.asList(new Product(1, "Applle", new BigDecimal(1200.00),true),
                 new Product(2, "Orange", new BigDecimal(500),false)
         );
